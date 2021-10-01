9c4056c5c2b8158a0ca470ab5ea4ec7de26f4e57
Kiran Samant <kiran.samant@capgemini.com>
2021-10-01 16:24:16 +0530

    file running sucessfullygit status

--- binarysearch.js
+++ binarysearch.js
@@ -1,37 +1,39 @@
+var a = [
+    1,
+    2,
+    4,
+    6,
+    1,
+    100,
+    0,
+    10000,
+    3
+];
 
-let recursiveFunction = function (arr, x, start, end) {
-      
-    // Base Condition
-    if (start > end) return false;
-  
-    // Find the middle index
-    let mid=Math.floor((start + end)/2);
-  
-    // Compare mid with given key x
-    if (arr[mid]===x) return true;
-         
-    // If element at mid is greater than x,
-    // search in the left half of mid
-    if(arr[mid] > x)
-        return recursiveFunction(arr, x, start, mid-1);
-    else
- 
-        // If element at mid is smaller than x,
-        // search in the right half of mid
-        return recursiveFunction(arr, x, mid+1, end);
+a.sort(function (a, b) {
+    return a - b;
+});
+
+console.log('a,', a);
+
+function binarySearch(arr, i) {
+    var mid = Math.floor(arr.length / 2);
+    console.log(arr[mid], i);
+
+    if (arr[mid] === i) {
+        console.log('match', arr[mid], i);
+        return arr[mid];
+    } else if (arr[mid] < i && arr.length > 1) {
+        console.log('mid lower', arr[mid], i);
+        binarySearch(arr.splice(mid, Number.MAX_VALUE), i);
+    } else if (arr[mid] > i && arr.length > 1) {
+        console.log('mid higher', arr[mid], i);
+        binarySearch(arr.splice(0, mid), i);
+    } else {
+        console.log('not here', i);
+        return -1;
+    }
+
 }
-  
- //Driver code
-let arr = [1, 3, 5, 7, 8, 9];
-let x = 5;
-  
-if (recursiveFunction(arr, x, 0, arr.length-1))
-    document.write("Element found!<br>");
-else document.write("Element not found!<br>");
-  
-x = 6;
-  
-if (recursiveFunction(arr, x, 0, arr.length-1))
-    document.write("Element found!<br>");
-else document.write("Element not found!<br>"); 
-     
+var result = binarySearch(a, 100);
+console.log(result);
